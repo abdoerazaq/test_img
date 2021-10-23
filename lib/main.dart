8a77b9b723a35d9ed35e568fdfe712aea77764d9
FlutLab.io <>
2021-10-23 07:33:51 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
           width: 200,
           height: 200,
           padding: EdgeInsets.all(3),
-          child: Image(image, NetworkImage("https://img.fotocommunity.com/wolfgangsee-42a663fb-0bb6-46d0-b0ba-25e6289cf576.jpg?height=1080")),
+          child: Image(
+            image,
+            NetworkImage("https://img.fotocommunity.com/wolfgangsee-42a663fb-0bb6-46d0-b0ba-25e6289cf576.jpg?height=1080"),
+          ),
         ), //container
       ), //scaffold
     ); //Material
