5590ba8e7d546cf0bb1fc324d5672a36d3bceb30
FlutLab.io <>
2021-10-23 07:38:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        child: Container(
-          width: 200,
-          height: 200,
-          padding: EdgeInsets.all(3),
-          child: Image(
-            image,
-            NetworkImage("https://img.fotocommunity.com/wolfgangsee-42a663fb-0bb6-46d0-b0ba-25e6289cf576.jpg?height=1080"),
-          ),
-        ), //container
+        appBar: AppBar(
+          title: Text("latihan"),
+        ),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            padding: EdgeInsets.all(3),
+            child: Image(
+              image: NetworkImage("https://img.fotocommunity.com/wolfgangsee-42a663fb-0bb6-46d0-b0ba-25e6289cf576.jpg?height=1080"),
+            ),
+          ), //container
+        ), //center
       ), //scaffold
     ); //Material
   } //widget build
